c3327f38713ab3c7aff05ac03abfb32e5c099e1c
Suu01 <92988188+Suu01@users.noreply.github.com>
2023-05-21 18:21:53 +0900

    using-promise

---
+++ 8/8_3_app.js
@@ -0,0 +1,13 @@
+const amISexy = new Promise((resolve, reject) => {
+    // setTimeout(resolve, 3000, "Yes you are!");
+    // resolve("Yes you are");
+    setTimeout(reject, 3000, "Yes you are!");
+});
+
+const thenFn = value => console.log(value);
+
+// amISexy.then(value => console.log(value));
+// amISexy.then(thenFn); 
+amISexy
+    .then(result => console.log(value))
+    .catch(error => console.log(value));
